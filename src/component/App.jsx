c23e8d25230b2cc60e5ab78f26548d603aa36c19
chhavi6V <chhavigupta597@gmail.com>
2023-06-22 19:09:01 +0530

    2nd commit

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -3,12 +3,18 @@ import reactDOM from 'react-dom';
 import Header from './Header'
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes'
 
 function App(){
   return(
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+          <Note 
+            title =  {note.title}
+            content = {note.content}
+          />
+      ))}
       <Footer />
     </div>
   )
